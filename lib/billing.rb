6671f1bd1778a76ceefdf892d46ee2d8c4e199be
Gabriel Sugai <gabrielsugai@gmail.com>
2021-05-09 01:18:46 -0300

    Adiciona valor as cobrancas

--- lib/billing.rb
+++ lib/billing.rb
@@ -1,10 +1,11 @@
 class Billing
-  attr_accessor :token, :payment_method, :payment_token, :expiration
+  attr_accessor :token, :payment_method, :payment_token, :value, :expiration
 
-  def initialize(token:, payment_method:, payment_token:, expiration:)
+  def initialize(token:, payment_method:, payment_token:,value:, expiration:)
     @token = token
     @payment_method = payment_method
     @payment_token = payment_token
+    @value = value
     @expiration = expiration
   end
 end
--- spec/billing_spec.rb
+++ spec/billing_spec.rb
@@ -5,12 +5,14 @@ describe Billing do
     billing = Billing.new(token: '14f4d82cfda1bab13b2f57869024b524bf5142ca',
                           payment_method: 'credit_card',
                           payment_token: 'f97e19a69c572a533423fba90dc9f28c1aeefed3',
+                          value: 100,
                           expiration: '26/04/2033')
     
     expect(billing.class).to eq Billing
     expect(billing.token).to eq('14f4d82cfda1bab13b2f57869024b524bf5142ca')
     expect(billing.payment_method).to eq('credit_card')
     expect(billing.payment_token).to eq('f97e19a69c572a533423fba90dc9f28c1aeefed3')
+    expect(billing.value).to eq(100)
     expect(billing.expiration).to eq('26/04/2033')
   end
 end
